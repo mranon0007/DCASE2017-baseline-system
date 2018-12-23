ffaf0f7bab35a6e8bcd984b351b22963b8c5e974
mranon0007 <mranon0007@gmail.com>
2018-12-23 18:40:49 +0500

    CustomTask1 Updated

--- applications/customTask1.py
+++ applications/customTask1.py
@@ -1,23 +1,115 @@
-import ptvsd
-ptvsd.enable_attach(address = ('10.148.0.2', 3289), redirect_output=True)
-ptvsd.wait_for_attach()
+# import ptvsd
+# ptvsd.enable_attach(address = ('10.148.0.2', 3289), redirect_output=True)
+# ptvsd.wait_for_attach()
 
 import task1
 
-from cStringIO import StringIO
+# from cStringIO import StringIO
+# import sys
+
+# class Capturing(list):
+#     def __enter__(self):
+#         self._stdout = sys.stdout
+#         sys.stdout = self._stringio = StringIO()
+#         return self
+#     def __exit__(self, *args):
+#         self.extend(self._stringio.getvalue().splitlines())
+#         del self._stringio    # free up some memory
+#         sys.stdout = self._stdout
+
+# with Capturing() as output:
+#     task1.main(sys.argv)
+
+# print output
+
+import os
 import sys
+import threading
+import time
 
-class Capturing(list):
+class OutputGrabber(object):
+    """
+    Class used to grab standard output or another stream.
+    """
+    escape_char = "\b"
+
+    def __init__(self, stream=None, threaded=False):
+        self.origstream = stream
+        self.threaded = threaded
+        if self.origstream is None:
+            self.origstream = sys.stdout
+        self.origstreamfd = self.origstream.fileno()
+        self.capturedtext = ""
+        # Create a pipe so the stream can be captured:
+        self.pipe_out, self.pipe_in = os.pipe()
+
     def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
+        self.start()
         return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
 
-with Capturing() as output:
-    task1.main(sys.argv)
+    def __exit__(self, type, value, traceback):
+        self.stop()
 
-print output
+    def start(self):
+        """
+        Start capturing the stream data.
+        """
+        self.capturedtext = ""
+        # Save a copy of the stream:
+        self.streamfd = os.dup(self.origstreamfd)
+        # Replace the original stream with our write pipe:
+        os.dup2(self.pipe_in, self.origstreamfd)
+        if self.threaded:
+            # Start thread that will read the stream:
+            self.workerThread = threading.Thread(target=self.readOutput)
+            self.workerThread.start()
+            # Make sure that the thread is running and os.read() has executed:
+            time.sleep(0.01)
+
+    def stop(self):
+        """
+        Stop capturing the stream data and save the text in `capturedtext`.
+        """
+        # Print the escape character to make the readOutput method stop:
+        self.origstream.write(self.escape_char)
+        # Flush the stream to make sure all our data goes in before
+        # the escape character:
+        self.origstream.flush()
+        if self.threaded:
+            # wait until the thread finishes so we are sure that
+            # we have until the last character:
+            self.workerThread.join()
+        else:
+            self.readOutput()
+        # Close the pipe:
+        os.close(self.pipe_in)
+        os.close(self.pipe_out)
+        # Restore the original stream:
+        os.dup2(self.streamfd, self.origstreamfd)
+        # Close the duplicate stream:
+        os.close(self.streamfd)
+
+    def readOutput(self):
+        """
+        Read the stream data (one byte at a time)
+        and save the text in `capturedtext`.
+        """
+        while True:
+            char = os.read(self.pipe_out, 1)
+            if not char or self.escape_char in char:
+                break
+            self.capturedtext += char
+
+# out = OutputGrabber()
+# with out:
+#     task1.main(sys.argv)
+#     print("+++++++++++++++++++++++++++++=")
+#     print(out.capturedtext)
+#     print("+++++++++++++++++++++++++++++=")
+
+out = OutputGrabber()
+out.start()
+task1.main(sys.argv)
+out.stop()
+print("+++++++++++++++++++++++++++++=")
+print(out.capturedtext)
