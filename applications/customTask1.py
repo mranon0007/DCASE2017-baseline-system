5d1fefeb21ff7aa448437e4c9c9988722daa0f10
mranon0007 <mranon0007@gmail.com>
2019-01-25 20:36:39 +0500

    Sync

--- applications/customTask1.py
+++ applications/customTask1.py
@@ -1,13 +1,13 @@
-# import ptvsd
-# ptvsd.enable_attach(address = ('10.148.0.2', 3289), redirect_output=True)
-# ptvsd.wait_for_attach()
-
 import task1
 
 import os
 import sys
 import threading
 import time
+
+import ptvsd
+ptvsd.enable_attach(address = ('10.148.0.2', 3289), redirect_output=True)
+ptvsd.wait_for_attach()
 
 dirname = os.path.dirname(__file__)
 
