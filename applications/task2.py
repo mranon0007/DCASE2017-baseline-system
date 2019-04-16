1fd6f221ad51ff45ba59ef085d64ae5b1b07f21e
mranon0007 <mranon0007@gmail.com>
2019-04-16 21:30:42 +0500

    Sync

--- applications/task2.py
+++ applications/task2.py
@@ -18,6 +18,7 @@ from dcase_framework.application_core import BinarySoundEventAppCore
 from dcase_framework.parameters import ParameterContainer
 from dcase_framework.utils import *
 from dcase_framework.features import FeatureExtractor
+from dcase_framework.learners import SceneClassifier
 
 __version_info__ = ('1', '0', '0')
 __version__ = '.'.join(__version_info__)
