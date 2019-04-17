e46d9010c5f5385c7de3d9385f5fd426fc6ad310
mranon0007 <mranon0007@gmail.com>
2019-04-17 07:41:37 +0500

    Sync

--- examples/task1Api.py
+++ examples/task1Api.py
@@ -107,7 +107,8 @@ print(Task1Output[0].split(':')[1])
 eval_file = Task1Output[0].split(':')[1] + "/results_fold1.txt"
 
 with open(eval_file, 'r') as stream:
-    stream = [ x.strip().split("\t") for x in stream.readlines()]
+    stream_lines = stream.readlines()
+    stream = [ x.strip().split("\t") for x in stream_lines ]
     pass
 # #return the output.
 print(time.clock() - start)
