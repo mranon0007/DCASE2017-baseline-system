8c297e340956f5f67096df0fbd29e15d701ed2a2
mranon0007 <mranon0007@gmail.com>
2019-04-17 11:02:49 +0500

    Sync

--- examples/task1Api.php
+++ examples/task1Api.php
@@ -1,5 +1,6 @@
 <?php
 
+echo dirname(__FILE__);
 $x = 12222;
 // $x = exec("python examples/task1Api.py b085_110_120.wav");
-file_put_contents("x.txt", $x);
+// file_put_contents(dirname(__FILE__)."/x.txt", $x);
