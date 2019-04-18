42d02d974456bd90b863d35cae05f543490c8120
mranon0007 <mranon0007@gmail.com>
2019-04-18 06:25:06 +0500

    Sync

--- examples/custom_task1.py
+++ examples/custom_task1.py
@@ -453,6 +453,8 @@ class MyLayerLSTM(Layer):
         super(MyLayerLSTM, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
+        l = x.numpy()
+        return l.reshape(40,100)[:,0:40].reshape(2400)
         return self.my_lambda_func(x)
 
     def my_func(self, x):
