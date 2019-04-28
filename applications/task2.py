b4d18707dcba3b37aa144bce009daf9d93ec42f3
mranon0007 <mranon0007@gmail.com>
2019-04-28 12:37:34 +0500

    Sync

--- applications/task2.py
+++ applications/task2.py
@@ -399,7 +399,7 @@ class EventDetectorCNNLSTM(EventDetector):
         #CNN
         # cnn_reshaper = Reshape(X1_Shape)(X1)
 
-        cl_c_reshaper = Lambda((lambda x: x[:,:,60:100,1]), output_shape=((40,40,1),))(cnn_reshaper)
+        cl_c_reshaper = Lambda((lambda x: x[:,:,59:99,1]), output_shape=((40,40,1),))(cnn_reshaper)
         #add layer to reshape (40,100,1) to (40,60:100,1) #first 60 columns are MFCCS
 
         conv1         = Conv2D(conv1_filters, kernel_size=conv1_kernel_size, activation='relu')(cl_c_reshaper)
